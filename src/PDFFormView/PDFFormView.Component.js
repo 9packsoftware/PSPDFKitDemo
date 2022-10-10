5203605bcd14f0baf78e0c1a4547586f14b6e8f9
Rafael <nemesysolano@gmail.com>
2022-10-10 11:28:53 -0400

    Polished refactoring

--- src/PDFFormView/PDFFormView.Component.js
+++ src/PDFFormView/PDFFormView.Component.js
@@ -1,48 +1,13 @@
 import React from 'react'
 
-import {PDFFormViewServices} from './PDFFormView.Services.js';
-
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
  import {
    View,
    Button
  } from 'react-native';
  import { containerStyle, saveButtonStyle, pdfViewStyle } from './PDFFormView.style'
+ import { PDFFormViewServices, FORM_PATH_IN_DOCUMENTS } from './PDFFormView.Services.js';
+
  import PSPDFKitView from 'react-native-pspdfkit';
- import RNFS from "react-native-fs";
- 
- const BASE_FILE_NAME = "work-order-form"
- const FORM_TEMPLATE_FILE_NAME = `${BASE_FILE_NAME}.pdf`
- const FORM_PATH_IN_DOCUMENTS = RNFS.DocumentDirectoryPath + FORM_TEMPLATE_FILE_NAME;
- 
- const onSaveButtonClick = (self, event) => {
-   self.refs.pdfView
-   .saveCurrentDocument()
-   .then(saved => {
-     if (saved) {
-       alert('Successfully saved current document.');
-     } else {
-       alert('Document was not saved as it was not modified.');
-     }
-   })
-   .catch(error => {
-     alert(JSON.stringify(error));
-   });
- }
- 
- const copyFormToDocuments = async () => {
-     const exists = await RNFS.exists(FORM_PATH_IN_DOCUMENTS);
-     if (!exists){
-       await RNFS.copyFileAssets(FORM_TEMPLATE_FILE_NAME, FORM_PATH_IN_DOCUMENTS);  
-     }  
- };
  
  const configuration = {
    showThumbnailBar: 'scrollable',
@@ -60,15 +25,14 @@ import {PDFFormViewServices} from './PDFFormView.Services.js';
    }
  
    componentDidMount() {
-     (async (self) => {
-       copyFormToDocuments();
-       self.setState({templateLoaded: true});
-     })(this)
+    (async () =>
+        await PDFFormViewServices.loadFormTemplate(this)
+    )()
    }
  
    render() {
      const self = this;
-     const onSaveButtonClickProxy = (event) => onSaveButtonClick(self, event);
+     const onSaveButtonClickProxy = (event) => PDFFormViewServices.onSaveFormRequest(self);
      const templateLoaded = this.state.templateLoaded
      return (
        templateLoaded ? 
--- src/PDFFormView/PDFFormView.Services.js
+++ src/PDFFormView/PDFFormView.Services.js
@@ -1,12 +1,30 @@
 import RNFS from "react-native-fs";
 
 const BASE_FILE_NAME = "work-order-form"
-const FORM_TEMPLATE_FILE_NAME = `${BASE_FILE_NAME}.pdf`
-const FORM_PATH_IN_DOCUMENTS = RNFS.DocumentDirectoryPath + FORM_TEMPLATE_FILE_NAME;
+export const FORM_TEMPLATE_FILE_NAME = `${BASE_FILE_NAME}.pdf`
+export const FORM_PATH_IN_DOCUMENTS = RNFS.DocumentDirectoryPath + FORM_TEMPLATE_FILE_NAME;
 
-const PDFFormViewServices = {
+export const PDFFormViewServices = {
     loadFormTemplate: async (self) => {
-        copyFormToDocuments();
+        const exists = await RNFS.exists(FORM_PATH_IN_DOCUMENTS);
+        if (!exists){
+          await RNFS.copyFileAssets(FORM_TEMPLATE_FILE_NAME, FORM_PATH_IN_DOCUMENTS);  
+        }     
         self.setState({templateLoaded: true});
-    }
+    },
+
+    onSaveFormRequest: (self) => {
+        self.refs.pdfView
+        .saveCurrentDocument()
+        .then(saved => {
+          if (saved) {
+            alert('Successfully saved current document.');
+          } else {
+            alert('Document was not saved as it was not modified.');
+          }
+        })
+        .catch(error => {
+          alert(JSON.stringify(error));
+        });
+      }
 }
